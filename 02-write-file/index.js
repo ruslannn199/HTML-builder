6a6f8267398ddb5bf5e77c386dcace969c4b20ae
ruslannn199 <ruslannn199@gmail.com>
2023-05-04 21:27:56 +0300

    feat: solve 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const { stdin, stdout, exit } = process;
+
+const output = fs.createWriteStream(path.join(__dirname, 'dest.txt'), 'utf-8');
+stdout.write('Введите текст:\n');
+stdin.on('data', data => {
+  if (data.toString().slice(0, -2) === 'exit'){
+    process.exit();
+  }
+  output.write(data);
+});
+
+process.on('exit', () => {
+  stdout.write('Всего доброго!');
+});
+
+process.on('SIGINT', exit);
