f7451fbacf8485f9ca631e5622fb9dbc1b9c013c
ruslannn199 <ruslannn199@gmail.com>
2023-05-07 18:47:55 +0300

    feat: solve 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,27 +1,28 @@
 const path = require('path');
-const { rm ,copyFile, mkdir, readdir } = require('fs/promises');
+const { rm ,copyFile, mkdir, readdir, readFile, writeFile } = require('fs/promises');
+const { createWriteStream, createReadStream } = require('fs');
 
 const dist = path.join(__dirname, 'project-dist');
 
-try {
-  rm(dist, { recursive: true });
-}
-catch (err) {
-}
-finally {
-  mkdir(dist, { recursive: true });
-}
+const createProjectFolder = async() => {
+  try {
+    await rm(dist, { recursive: true });
+  }
+  catch (err) {
+    console.log('Папка project-dist создана');
+  }
+  finally {
+    await mkdir(dist, { recursive: true });
+  }
+};
 
-copyDir = async(src, input = dist, output = dist) => {
+const copyDir = async(src, input = __dirname, output = dist) => {
   const folder = path.join(input, src);
   const copyFolder = path.join(output, src);
   try {
     await rm(copyFolder, { recursive: true });
   }
   catch (err) {
-    console.log(err);
-  }
-  finally {
     await mkdir(copyFolder, { recursive: true });
   }
 
@@ -36,4 +37,34 @@ copyDir = async(src, input = dist, output = dist) => {
   }
 };
 
-copyDir('assets');
+
+const createStyles = async(src) => {
+  const styles = createWriteStream(path.join(dist, 'style.css'));
+  const dataArr = await readdir(path.join(__dirname, src), { withFileTypes: true });
+
+  for (const data of dataArr) {
+    if (data.isFile() && path.extname(data.name).slice(1) === 'css') {
+      createReadStream(path.join(__dirname, src, data.name)).pipe(styles);
+    }
+  }
+};
+
+const createHTML = async(src) => {
+  let templateHTML = await readFile(path.join(__dirname, 'template.html'), 'utf-8');
+  const componentsArr = templateHTML.matchAll(/{{(.*?)}}/gi);
+
+  for (const component of componentsArr) {
+    const componentHTML = await readFile(path.join(__dirname, src, `${component[1]}.html`), 'utf-8');
+    templateHTML = templateHTML.replace(component[0], componentHTML);
+  }
+  await writeFile(path.join(dist, 'index.html'), templateHTML, 'utf-8');
+};
+
+const createBundle = async() => {
+  await createProjectFolder();
+  await copyDir('assets');
+  await createStyles('styles');
+  await createHTML('components');
+};
+
+createBundle();
