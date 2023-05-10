8b0950fd0fdd47639f6c8b7ab380e8f4e0488564
ruslannn199 <ruslannn199@gmail.com>
2023-05-10 22:12:45 +0300

    fix: fix extention issue

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,9 @@ const readFiles = async() => {
   const secretFolderArr = await readdir(secretFolder, { withFileTypes: true });
   for (const data of secretFolderArr) {
     if (data.isFile()) {
-      const file = path.basename(data.name).slice(0, path.basename(data.name).lastIndexOf('.'));
+      const file = path.basename(data.name).includes('.')
+        ? path.basename(data.name).slice(0, path.basename(data.name).lastIndexOf('.'))
+        : path.basename(data.name);
       const ext = path.extname(data.name).slice(1);
       const size = parseFloat((await stat(path.join(secretFolder, data.name))).size / 1024).toFixed(3);
       console.log(`${file} - ${ext} - ${size} KB`);
